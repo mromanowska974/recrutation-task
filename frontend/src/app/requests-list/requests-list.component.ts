511a99d5eb91b7e108a0e706c271e15e87649bff
mromanowska974 <m.romanowska974@gmail.com>
2024-12-06 11:49:55 +0000

    Displaying all requests
    All requests made by users can be now displayed in the app. Requests are retrieved from database.

--- frontend/src/app/requests-list/requests-list.component.ts
+++ frontend/src/app/requests-list/requests-list.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { DataRequest } from '../dataRequest';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-requests-list',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './requests-list.component.html',
   styleUrl: './requests-list.component.css'
 })
-export class RequestsListComponent {
+export class RequestsListComponent implements OnInit{
+  apiService = inject(ApiService);
 
+  allRequests: DataRequest[] = [];
+
+  ngOnInit(): void {
+      this.apiService.getAllRequests().subscribe(requests => {
+        this.allRequests = Object.values(requests).map(request => {
+          let item: DataRequest = JSON.parse(JSON.stringify(request));
+          return item;
+        });
+      });
+  }
 }
